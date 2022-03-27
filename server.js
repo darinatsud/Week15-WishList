5cdf31c4ed2f0bb9fba6089db634955f02b245c6
Darina Tšudakova <dtsudako@tktk.ee>
2022-03-27 22:04:08 +0300

    my comment

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ app.post('/wish', (req, res) => {
 
 const port = 5000;
 
-app.listen(port, () => {
-    console.log(`Server is running ${port}.`);
+app.listen(process.env.PORT || 5000, function() {
+    console.log(`Server has started.`);
 });
